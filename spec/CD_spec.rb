cfecc2f7a5b465b4ff984280a3b8f1d9d23ce05f
Alexa Zeazas & Travis Crampton <student@epicodus.com>
2015-08-12 14:21:19 -0700

    Add CD specs.

--- spec/CD_spec.rb
+++ spec/CD_spec.rb
@@ -2,9 +2,9 @@ require('rspec')
 require('CD')
 
 describe('CD') do
-  # before() do
-  #   CD.clear
-  # end
+  before() do
+    CD.clear
+  end
 
   describe('#title') do
     it('returns the title of the CD') do
@@ -13,7 +13,7 @@ describe('CD') do
       expect(test_CD.title()).to(eq("Turtleneck & Chain"))
     end
   end
-  describe("#artist")
+  describe("#artist") do
     it('returns the artist of the CD') do
       test_CD = CD.new("Turtleneck & Chain", "The Lonely Island")
       test_CD.save()
@@ -23,6 +23,29 @@ describe('CD') do
   describe(".all") do
     it('is empty at first') do
       expect(CD.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it('adds a CD to the array of saved CDs') do
+      test_CD = CD.new("Turtleneck & Chain", "The Lonely Island")
+      test_CD.save()
+      expect(CD.all()).to(eq([test_CD]))
+    end
+  end
+  describe(".clear") do
+    it("empties out all of the saved CDs") do
+      CD.new("Turtleneck & Chain", "The Lonely Island")
+      CD.clear()
+      expect(CD.all()).to(eq([]))
+    end
+  end
+  describe(".find") do
+    it("returns a CD by its id number") do
+      test_CD = CD.new("Turtleneck & Chain", "The Lonely Island")
+      test_CD.save()
+      test_CD2 = CD.new("A Night at the Opera", "Queen")
+      test_CD2.save()
+      expect(CD.find(test_CD.id())).to(eq(test_CD))
     end
   end
 end
